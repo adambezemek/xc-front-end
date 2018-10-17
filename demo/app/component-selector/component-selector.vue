e9dca1a7f70302868986dedd2063d5df3ff2b50e
Adam Mornes <Adam.Mornes@xcentium.com>
2018-10-17 16:04:26 -0500

    Cleaned up styling for component selecto

--- demo/app/component-selector/component-selector.vue
+++ demo/app/component-selector/component-selector.vue
@@ -1,26 +1,18 @@
 <template>
-    <div class="xc-cl-component-selector">
-        <div class="xc-cl-component-selector__controls">
-            <label for=""></label>
-            <select v-model="template">
-                <option v-for="(component, index) in components" :value="index" :key="index">
-                    {{ index }}
-                </option>
-            </select>
-        </div>
+    <div class="xc-cl-component-selector" @mouseover="checkVisibility" @mouseleave="checkVisibility">
+        <transition name="fade">
+            <div class="xc-cl-component-selector__controls" v-if="isVisible">
+                <label class="xc-cl-component-selector__label" for="componentSelect">Select Component:</label>
+                <select class="xc-cl-component-selector__select" v-model="template" id="componentSelect" @focus="isEditing = true" @blur="checkVisibility">
+                    <option v-for="(component, index) in components" :value="index" :key="index">
+                        {{ index }}
+                    </option>
+                </select>
+            </div>
+        </transition>
         <div class="xc-cl-component-selector__wrapper" v-html="components[template]"></div>
     </div>
 </template>
-
-<style lang="scss">
-    .xc-cl-component-selector {
-        overflow: hidden;
-
-        &__wrapper {
-            overflow: hidden;
-        }
-    }
-</style>
 
 <script>
     export default {
@@ -30,11 +22,31 @@
         },
         data: () => {
             return {
-                template: ''
+                template: '',
+                isVisible: false,
+                isHover: false,
+                isEditing: false
             }
         },
         created: function() {
             this.template = this.defaultComponent;
+        },
+        methods: {
+            checkVisibility(e) {
+                if(e.type === 'mouseover') {
+                    this.isHover = true;
+                    this.isVisible = true;
+                } else {
+                    if(e.type === 'mouseleave') {
+                        this.isHover = false;
+                    } else if(e.type === 'blur') {
+                        this.isEditing = false;
+                    }
+                    if(!this.isHover && !this.isEditing) {
+                        this.isVisible = false;
+                    }
+                }
+            }
         }
     }
 </script>
